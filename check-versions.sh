0c2608a680cbb05e2794ba89ce2504921122781a
Louis Moureaux <m_louis30@yahoo.com>
2024-01-04 18:39:55 +0100

    Ensure the exit code is 0 on success and 1 on error

--- check-versions.sh
+++ check-versions.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText:  Tobias Rehbein <tobias.rehbein@web.de>
+# SPDX-FileCopyrightText: Tobias Rehbein <tobias.rehbein@web.de>
+# SPDX-FileCopyrightText: Louis Moureaux <m_louis30@yahoo.com>
 
 # Checks that the version numbers in the index file match those in the
 # modpacks.
 
 IFS='|'
 jq -r '.modpacks[] | "\(.name)|\(.version)|\(.url)"' index.json \
-	| while read NAME VERSION URL; do
-		RVERSION=$({ if $(echo "$URL" | grep -q '^http'); then
-			curl -s "$URL"
-		else
-			cat "$URL"
-		fi } | jq -r '.info.version')
+	| {
+		STATUS=0
+		while read NAME VERSION URL; do
+			RVERSION=$({ if $(echo "$URL" | grep -q '^http'); then
+				curl -s "$URL"
+			else
+				cat "$URL"
+			fi } | jq -r '.info.version')
 
-		[ "$VERSION" != "$RVERSION" ] \
-			&& echo "Version mismatch for \"$NAME\": index $VERSION; remote $RVERSION"
-	done
-
+			[ "$VERSION" != "$RVERSION" ] \
+				&& STATUS=1 \
+				&& echo "Version mismatch for \"$NAME\": index $VERSION; remote $RVERSION"
+		done
+		exit $STATUS
+	}
